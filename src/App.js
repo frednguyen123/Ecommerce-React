b3ef677060d47fd82dfd97bfa2b2df2bdc7108ec
frednguyen123 <fred.nguyen.2000@gmail.com>
2022-09-09 23:55:46 -0700

    Added BookInfo and Routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/books' element={<Books books={books}/>} />
+            <Route path='/books/1' element={<BookInfo books={books}/>} />
           </Routes>
           <Footer />
       </div>
